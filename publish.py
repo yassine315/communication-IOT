1f4d535dbd6452e3f6ac55da6fe8d4e7af61624f
yassine315 <yassim.lmahi@gmail.com>
2019-11-15 22:55:30 +0100

    communication de ESP-8266 avec proker en utilisant MQTT

---
+++ publish.py
@@ -0,0 +1,51 @@
+'''
+Created on 15 nov. 2019
+
+@author: Yassine
+'''
+
+import paho.mqtt.client as mqtt
+
+
+
+msg = "hello" # must be a string, bytearray, int, float
+def on_connect(mqttc, obj, flags, rc):
+    print("rc: " + str(rc))
+
+
+def on_message(mqttc, obj, msg):
+    print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+
+
+def on_publish(mqttc, obj, mid):
+    print("mid: " + str(mid))
+    pass
+
+
+def on_subscribe(mqttc, obj, mid, granted_qos):
+    print("Subscribed: " + str(mid) + " " + str(granted_qos))
+
+
+def on_log(mqttc, obj, level, string):
+    print(string)
+
+
+# If you want to use a specific client id, use
+# mqttc = mqtt.Client("client-id")
+# but note that the client id must be unique on the broker. Leaving the client
+# id parameter empty will generate a random id for you.
+mqttc = mqtt.Client()
+mqttc.on_message = on_message
+mqttc.on_connect = on_connect
+mqttc.on_publish = on_publish
+mqttc.on_subscribe = on_subscribe
+# Uncomment to enable debug messages
+# mqttc.on_log = on_log
+mqttc.connect("localhost", 1883, 60)
+
+mqttc.loop_start()
+
+print("class")
+infot = mqttc.publish("esp/test", msg, qos=2)
+
+infot.wait_for_publish()
